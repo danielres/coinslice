1ba28e02703bb1ea96dc194b8378d4d9d8a0c6a0
Daniel Reszka <dannn.r@gmail.com>
2017-11-30 22:55:47 +0100

    setup redux-thunk in store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,13 @@ import {
   compose as _compose,
 } from 'redux'
 
+import thunk from 'redux-thunk'
+
 const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || _compose
 
 export const rootReducer = combineReducers({})
 
-const middlewares = applyMiddleware()
+const middlewares = applyMiddleware(thunk)
 
 const store = createStore(rootReducer, compose(middlewares))
 
