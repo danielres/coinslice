8b029831283c793ab1f61ab77f9706979643333b
Daniel Reszka <dannn.r@gmail.com>
2017-12-03 02:46:43 +0100

    add form validation (without UX)

--- src/PortfolioForm/index.js
+++ src/PortfolioForm/index.js
@@ -4,11 +4,17 @@ import { connect } from 'react-redux'
 import { updateForm, resetForm } from '../store/portfolioForm/actions'
 import { addEntry, removeEntry } from '../store/portfolio/actions'
 import PortfolioForm from './PortfolioForm'
+import validateForm from '../utils/validateForm'
+
+import { PortfolioNewEntrySchema } from '../schemas'
 
 export default connect(
   ({ portfolioForm }) => ({ portfolioForm }),
   dispatch => ({
-    onSubmit: entry => dispatch(addEntry(entry)),
+    onSubmit: entry => {
+      const errors = validateForm(entry, PortfolioNewEntrySchema)
+      errors ? console.error(errors) : dispatch(addEntry(entry))
+    },
     updateForm: ({ target: { name, value } }) =>
       dispatch(updateForm({ [name]: value })),
   })
